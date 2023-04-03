b3b0a11225a98a231f0acccb0f7d032f6715c2af
vijayusj <kinneravijay9@gmail.com>
2023-04-03 19:59:02 +0530

    added icons& slove sidebarBtn issue

--- js/DisplayNotes.js
+++ js/DisplayNotes.js
@@ -1,11 +1,14 @@
 export default class DisplayNotes {
-    constructor(root, { note_selection, note_addition, note_edit, note_delete } = {}) {
-        this.root = root;
-        this.note_selection = note_selection;
-        this.note_addition = note_addition;
-        this.note_edit = note_edit;
-        this.note_delete = note_delete;
-        this.root.innerHTML = `
+  constructor(
+    root,
+    { note_selection, note_addition, note_edit, note_delete } = {}
+  ) {
+    this.root = root;
+    this.note_selection = note_selection;
+    this.note_addition = note_addition;
+    this.note_edit = note_edit;
+    this.note_delete = note_delete;
+    this.root.innerHTML = `
             <div class="sidebar">
                 <div class="your__notes">Your Notes</div>
                 <div class="sidebar_empty_msg">
@@ -24,39 +27,36 @@ export default class DisplayNotes {
                 <p class="speech_instructions">Ready</p>
 
                 <div class="action_buttons">
-                    <button class="speech-start" type="button">Speak</button>
+                    <button class="speech-start" type="button"><i class="fa fa-microphone"></i></button>
                     <button class="speech-stop" type="button">Stop</button>
-                    <button class="btnAdd" type="button">New Note</button>
+                    <button class="btnAdd" type="button"><i class="fa fa-square-plus"></i></button>
                 </div>
             </div>
         `;
 
-        const addBtn = this.root.querySelector(".btnAdd");
-        const note_title = this.root.querySelector(".note_title");
-        const note_body = this.root.querySelector(".note_body");
+    const addBtn = this.root.querySelector('.btnAdd');
+    const note_title = this.root.querySelector('.note_title');
+    const note_body = this.root.querySelector('.note_body');
 
-        addBtn.addEventListener("click", () => {
-            this.note_addition();
-            // console.log("ADD");
-        });
+    addBtn.addEventListener('click', () => {
+      this.note_addition();
+      // console.log("ADD");
+    });
 
-        [note_title, note_body].forEach(inputField => {
+    [note_title, note_body].forEach((inputField) => {
+      inputField.addEventListener('blur', () => {
+        const timestamp_title = note_title.value.trim();
+        const timestamp_body = note_body.value.trim();
+        this.note_edit(timestamp_title, timestamp_body);
+      });
+    });
 
-            inputField.addEventListener("blur", () => {
+    this.update_preview_visibility(false);
+  }
 
-                const timestamp_title = note_title.value.trim();
-                const timestamp_body = note_body.value.trim();
-                this.note_edit(timestamp_title, timestamp_body);
-            });
-        });
-
-        this.update_preview_visibility(false);
-
-    }
-
-    create_list_item(id, title, body, timestamp) {
-        const MAX_SIDEBAR_BODY_LENGTH = 30;
-        return `
+  create_list_item(id, title, body, timestamp) {
+    const MAX_SIDEBAR_BODY_LENGTH = 30;
+    return `
             <div class="list_item" data-note-id="${id}">
 
                 <div class="small_title">
@@ -67,68 +67,81 @@ export default class DisplayNotes {
                 </div>
                 <div class="small_body">
                     ${body.substring(0, MAX_SIDEBAR_BODY_LENGTH)}
-                    ${body.length > MAX_SIDEBAR_BODY_LENGTH ? "..." : ""}
+                    ${body.length > MAX_SIDEBAR_BODY_LENGTH ? '...' : ''}
                 </div>
                 <div class="timestamp">
-                    ${timestamp.toLocaleString(undefined, { dateStyle: "full", timeStyle: "short" })}
+                    ${timestamp.toLocaleString(undefined, {
+                      dateStyle: 'full',
+                      timeStyle: 'short',
+                    })}
                 </div>
             </div>
         `;
+  }
+
+  update_note_sidebar_list(notes) {
+    const list_container = this.root.querySelector('.notes_list');
+
+    // Empty list
+    list_container.innerHTML = '';
+
+    for (const note of notes) {
+      const html = this.create_list_item(
+        note.id,
+        note.title,
+        note.body,
+        new Date(note.timestamp)
+      );
+
+      list_container.insertAdjacentHTML('beforeend', html);
     }
 
-    update_note_sidebar_list(notes) {
-        const list_container = this.root.querySelector(".notes_list");
+    // Add select/delete events for each list item
+    list_container.querySelectorAll('.list_item').forEach((list_item) => {
+      list_item.addEventListener('click', () => {
+        this.note_selection(list_item.dataset.noteId);
+      });
+    });
 
-        // Empty list
-        list_container.innerHTML = "";
+    list_container.querySelectorAll('.delBtn').forEach((deleteBtn) => {
+      deleteBtn.addEventListener('click', (event) => {
+        event.stopPropagation(); // Prevent the click event from bubbling up
+        const flag = confirm('Are you sure you want to delete this note?');
 
-        for (const note of notes) {
-            const html = this.create_list_item(note.id, note.title, note.body, new Date(note.timestamp));
-
-            list_container.insertAdjacentHTML("beforeend", html);
+        if (flag) {
+          const noteId = event.target.closest('.list_item').dataset.noteId;
+          this.note_delete(noteId);
         }
+      });
+    });
+  }
 
-        // Add select/delete events for each list item
-        list_container.querySelectorAll(".list_item").forEach(list_item => {
+  ret_active_note(note) {
+    this.root.querySelector('.note_title').value = note.title;
+    this.root.querySelector('.note_body').value = note.body;
 
-            list_item.addEventListener("click", () => {
-                this.note_selection(list_item.dataset.noteId);
-            });
-        });
+    this.root.querySelectorAll('.list_item').forEach((list_item) => {
+      list_item.classList.remove('list_item--selected');
+    });
 
-        list_container.querySelectorAll(".delBtn").forEach(deleteBtn => {
+    this.root
+      .querySelector(`.list_item[data-note-id="${note.id}"]`)
+      .classList.add('list_item--selected');
+  }
 
-            deleteBtn.addEventListener("click", event => {
-
-                event.stopPropagation(); // Prevent the click event from bubbling up
-                const flag = confirm("Are you sure you want to delete this note?");
-
-                if (flag) {
-                    const noteId = event.target.closest(".list_item").dataset.noteId;
-                    this.note_delete(noteId);
-                }
-            });
-        });
-    }
-
-    ret_active_note(note) {
-        this.root.querySelector(".note_title").value = note.title;
-        this.root.querySelector(".note_body").value = note.body;
-
-        this.root.querySelectorAll(".list_item").forEach(list_item => {
-            list_item.classList.remove("list_item--selected");
-        });
-
-        this.root.querySelector(`.list_item[data-note-id="${note.id}"]`).classList.add("list_item--selected");
-    }
-
-    update_preview_visibility(visible) {
-        this.root.querySelector(".note_title").style.display = visible ? "block" : "none";
-        this.root.querySelector(".note_body").style.display = visible ? "block" : "none";
-        this.root.querySelector(".empty_msg").style.display = visible ? "none" : "block";
-        this.root.querySelector(".sidebar_empty_msg").style.display = visible ? "none" : "block";
-        // this.root.querySelector(".preview").style.display = visible ? "inline" : "block";
-
-    }
-
+  update_preview_visibility(visible) {
+    this.root.querySelector('.note_title').style.display = visible
+      ? 'block'
+      : 'none';
+    this.root.querySelector('.note_body').style.display = visible
+      ? 'block'
+      : 'none';
+    this.root.querySelector('.empty_msg').style.display = visible
+      ? 'none'
+      : 'block';
+    this.root.querySelector('.sidebar_empty_msg').style.display = visible
+      ? 'none'
+      : 'block';
+    // this.root.querySelector(".preview").style.display = visible ? "inline" : "block";
+  }
 }
